e7c6aadc4b793489779da75eab7f33e9937fe57f
borgesribeiro-dev <borges.ribeiro@unifesp.br>
2026-01-08 21:40:56 -0300

    1100.c EDITADO.

--- 1100.c
+++ 1100.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
 
-#define N 8
+#define TAM_TABULEIRO 8
 
-int dist[N][N];
-int qx[N * N], qy[N * N];
+int distancia[TAM_TABULEIRO][TAM_TABULEIRO];
+int fila_x[TAM_TABULEIRO * TAM_TABULEIRO];
+int fila_y[TAM_TABULEIRO * TAM_TABULEIRO];
 
-int dx[8] = { 1,  2,  2,  1, -1, -2, -2, -1};
-int dy[8] = { 2,  1, -1, -2, -2, -1,  1,  2};
+int mov_x[8] = { 1,  2,  2,  1, -1, -2, -2, -1 };
+int mov_y[8] = { 2,  1, -1, -2, -2, -1,  1,  2 };
 
-int bfs(int sx, int sy, int tx, int ty) {
-    int ini = 0, fim = 0;
+int busca_largura(int x_ini, int y_ini, int x_fim, int y_fim) {
+    int inicio = 0, fim = 0;
 
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            dist[i][j] = -1;
+    for (int i = 0; i < TAM_TABULEIRO; i++)
+        for (int j = 0; j < TAM_TABULEIRO; j++)
+            distancia[i][j] = -1;
 
-    dist[sx][sy] = 0;
-    qx[fim] = sx;
-    qy[fim] = sy;
+    distancia[x_ini][y_ini] = 0;
+    fila_x[fim] = x_ini;
+    fila_y[fim] = y_ini;
     fim++;
 
-    while (ini < fim) {
-        int x = qx[ini];
-        int y = qy[ini];
-        ini++;
+    while (inicio < fim) {
+        int x_atual = fila_x[inicio];
+        int y_atual = fila_y[inicio];
+        inicio++;
 
-        if (x == tx && y == ty)
-            return dist[x][y];
+        if (x_atual == x_fim && y_atual == y_fim)
+            return distancia[x_atual][y_atual];
 
         for (int k = 0; k < 8; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
+            int novo_x = x_atual + mov_x[k];
+            int novo_y = y_atual + mov_y[k];
 
-            if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                if (dist[nx][ny] == -1) {
-                    dist[nx][ny] = dist[x][y] + 1;
-                    qx[fim] = nx;
-                    qy[fim] = ny;
+            if (novo_x >= 0 && novo_x < TAM_TABULEIRO &&
+                novo_y >= 0 && novo_y < TAM_TABULEIRO) {
+
+                if (distancia[novo_x][novo_y] == -1) {
+                    distancia[novo_x][novo_y] =
+                        distancia[x_atual][y_atual] + 1;
+
+                    fila_x[fim] = novo_x;
+                    fila_y[fim] = novo_y;
                     fim++;
                 }
             }
@@ -47,18 +52,24 @@ int bfs(int sx, int sy, int tx, int ty) {
 }
 
 int main(void) {
-    char a[3], b[3];
+    char origem[3], destino[3];
 
-    while (scanf("%2s %2s", a, b) == 2) {
-        int sx = a[0] - 'a';
-        int sy = a[1] - '1';
-        int tx = b[0] - 'a';
-        int ty = b[1] - '1';
+    while (scanf("%2s %2s", origem, destino) == 2) {
+        int x_inicio = origem[0] - 'a';
+        int y_inicio = origem[1] - '1';
+        int x_destino = destino[0] - 'a';
+        int y_destino = destino[1] - '1';
 
-        int moves = bfs(sx, sy, tx, ty);
+        int movimentos = busca_largura(
+            x_inicio, y_inicio, x_destino, y_destino
+        );
 
-        printf("To get from %s to %s takes %d knight moves.\n", a, b, moves);
+        printf(
+            "To get from %s to %s takes %d knight moves.\n",
+            origem, destino, movimentos
+        );
     }
 
     return 0;
 }
+
