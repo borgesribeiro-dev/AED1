4b213d87a9777c4752c090705dd052a0fc8a9281
borgesribeiro-dev <borges.ribeiro@unifesp.br>
2026-01-08 22:20:26 -0300

    1191.c EDITADO

--- 1191.c
+++ 1191.c
@@ -4,51 +4,41 @@
 
 #define MAXN 60
 
-typedef struct No {
+typedef struct no {
     char valor;
-    struct No *esq;
-    struct No *dir;
+    struct no *esq;
+    struct no *dir;
 } No;
 
 No *novo_no(char v) {
-    No *p = (No *)malloc(sizeof(No));
-    if (!p) exit(1);
-    p->valor = v;
-    p->esq = p->dir = NULL;
-    return p;
+    No *n = (No *)malloc(sizeof(No));
+    if (!n) exit(1);
+    n->valor = v;
+    n->esq = NULL;
+    n->dir = NULL;
+    return n;
 }
 
-int acha_pos(const char *in, int ini, int fim, char ch) {
+int busca(const char *in, int ini, int fim, char c) {
     for (int i = ini; i <= fim; i++) {
-        if (in[i] == ch) return i;
+        if (in[i] == c)
+            return i;
     }
     return -1;
 }
 
-No *construir(const char *pre, const char *in,
-              int pre_ini, int pre_fim,
-              int in_ini, int in_fim) {
-    if (pre_ini > pre_fim || in_ini > in_fim)
+No *constroi(const char *pre, const char *in, int pi, int pf, int ii, int ifim) {
+    if (pi > pf || ii > ifim)
         return NULL;
 
-    char raiz_val = pre[pre_ini];
-    int pos_raiz = acha_pos(in, in_ini, in_fim, raiz_val);
+    char r = pre[pi];
+    int pos = busca(in, ii, ifim, r);
 
-    No *raiz = novo_no(raiz_val);
+    No *raiz = novo_no(r);
+    int tam_esq = pos - ii;
 
-    int tam_esq = pos_raiz - in_ini;
-
-    raiz->esq = construir(pre, in,
-                          pre_ini + 1,
-                          pre_ini + tam_esq,
-                          in_ini,
-                          pos_raiz - 1);
-
-    raiz->dir = construir(pre, in,
-                          pre_ini + tam_esq + 1,
-                          pre_fim,
-                          pos_raiz + 1,
-                          in_fim);
+    raiz->esq = constroi(pre, in, pi + 1, pi + tam_esq, ii, pos - 1);
+    raiz->dir = constroi(pre, in, pi + tam_esq + 1, pf, pos + 1, ifim);
 
     return raiz;
 }
@@ -67,15 +57,13 @@ void libera(No *raiz) {
     free(raiz);
 }
 
-int main(void) {
+int main() {
     char pre[MAXN], in[MAXN];
 
     while (scanf("%s %s", pre, in) == 2) {
-        int n = (int)strlen(pre);
+        int n = strlen(pre);
 
-        No *raiz = construir(pre, in,
-                             0, n - 1,
-                             0, n - 1);
+        No *raiz = constroi(pre, in, 0, n - 1, 0, n - 1);
 
         posordem(raiz);
         putchar('\n');
@@ -85,3 +73,4 @@ int main(void) {
 
     return 0;
 }
+
